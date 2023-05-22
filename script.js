c3f2de68c3773d9656d008ebeb8571cead3446cd
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-22 09:55:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+
+var radiusInput = document.getElementById('radius');
+var calculateButton = document.getElementById('calculate');
+var volumeField = document.getElementById('volume');
+
+
 function volume_sphere() {
-    //Write your code here
+    var radius = parseFloat(radiusInput.value); 
+    var volume = (4/3) * Math.PI * Math.pow(radius, 3); 
+  
+    volumeField.textContent = "Volume: " + volume.toFixed(2);
+}
+
+
+calculateButton.addEventListener('click', volume_sphere);
+
   
 } 
 
