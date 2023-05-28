7723d0473523cbe62ed78d647f552891aa4798ba
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-28 14:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// Get the necessary elements from the HTML document
-var radiusInput = document.getElementById('radius');
-var calculateButton = document.getElementById('calculate');
-var volumeField = document.getElementById('volume');
 
-// Function to calculate the volume of a sphere
 function volume_sphere() {
-    var radius = parseFloat(radiusInput.value); // Get the radius value from the input field
-
-    // Check if the radius is a valid number
-    if (!isNaN(radius)) {
-        var volume = (4 / 3) * Math.PI * Math.pow(radius, 3); // Calculate the volume
-        volumeField.textContent = "Volume: " + volume.toFixed(2);
-    } else {
-        volumeField.textContent = "Invalid radius"; // Display an error message for an invalid radius
-    }
+	var volume;
+  var radius = document.getElementById('radius').value;
+   radius = Math.abs(radius);
+	 volume = (4 / 3) * Math.PI * Math.pow(radius, 3); 
+     volume = valume.tofixed(4);
+	document.getElementById('volume').value=valume;
+    return false;
 }
 
-
-calculateButton.addEventListener('click', volume_sphere);
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
