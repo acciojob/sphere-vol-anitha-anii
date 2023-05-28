968ce043a811f389dfd27602d1ad1ccd31da5809
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-28 14:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-function volume_sphere() {
-	var volume;
-  var radius = document.getElementById('radius').value;
-   radius = Math.abs(radius);
-	 volume = (4 / 3) * Math.PI * Math.pow(radius, 3); 
-     volume = valume.tofixed(4);
-	document.getElementById('volume').value=valume;
-    return false;
-}
+ function volume_sphere() {
+            var radius = document.getElementById('radius').value;
+            radius = Math.abs(radius);
+            var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            volume = volume.toFixed(4);
+            document.getElementById('volume').value = volume;
+            return false;
+        }
 
+        window.onload = function() {
+            document.getElementById('MyForm').onsubmit = volume_sphere;
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
